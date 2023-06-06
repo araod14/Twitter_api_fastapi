53b66131c8cd3dc2c7993c2bc074c426535d6c9d
Daniel Arao <araodaniel14@gmail.com>
2023-06-06 09:55:01 -0400

    tests tweests passed

--- test_main.py
+++ test_main.py
@@ -1,4 +1,4 @@
-#import json
+import json
 from main import app
 from fastapi.testclient import TestClient
 from config.db import session_local, engine
@@ -16,29 +16,37 @@ def override_get_db():
     finally:
         db.close()
 
+## Tweets
 tweets = [
     {
-        "content_tweet": "First tweet"
+  "tweet_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+  "content_tweet": "First tweet",
+  "created_at": "2023-06-05T13:05:12.120637",
+  "owner_id": "83dfed72-c2e3-4970-9b6c-ee05a7f67f2b"
     },
     {
-        "content_tweet": "Second tweet"
+  "tweet_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+  "content_tweet": "Second tweet",
+  "created_at": "2023-06-05T13:05:12.120637",
+  "owner_id": "83dfed72-c2e3-4970-9b6c-ee05a7f67f2b"
     }
 ]
+
 def test_post_tweet():
-    response = client.post("/users/1/post", json=tweets[0])
+    response = client.post("/users/83dfed72-c2e3-4970-9b6c-ee05a7f67f2b/post", json=tweets[0])
     assert response.status_code == 201
-    assert response.json()["content_tweet"] == "First tweet"
+    assert response.json()["content"] == "First tweet"
 
 def test_get_tweets():
     response = client.get("/tweets")
     assert response.status_code == 200
-    assert len(response.json()) == 2
-    assert response.json()[0]["content_tweet"] == "First tweet"
-    assert response.json()[1]["content_tweet"] == "Second tweet"
+    assert response.json()[0]["content"] == "string"
+    assert response.json()[1]["content"] == "string"
 
 def test_get_tweets_from_user():
-    response = client.get("/users/1/tweets")
+    response = client.get("/users/75d74717-f6ad-4ed6-91b0-44db56d4dbd4/tweets")
     assert response.status_code == 200
-    assert len(response.json()) == 2
+    assert len(response.json()) == 4
 
 
+## Users
